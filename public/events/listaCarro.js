f2a42f6e052e05b7ece0137df9e3acce35a1e05c
andrez.mazo@gmail.com <andrez.mazo@gmail.com>
2022-05-22 14:34:01 -0500

    carrito funcional

--- public/events/listaCarro.js
+++ public/events/listaCarro.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("load", function () {
   localStorage.removeItem("listaCarro");
-
+  // const body = document.querySelector("body");
   const root = document.getElementById("root");
   const container = document.createElement("div");
 
@@ -11,103 +11,233 @@ window.addEventListener("load", function () {
   root.appendChild(container);
 
   let productosEnCarro = localStorage.getItem("productosEnCarro");
-  let productos = JSON.parse("[" + productosEnCarro + "]");
 
-  var productosUnicos = productos.filter(
-    (value, index, self) => index === self.findIndex((t) => t.id === value.id)
-  );
+  if (
+    productosEnCarro != undefined &&
+    productosEnCarro != null &&
+    productosEnCarro != ""
+  ) {
+    let productos = JSON.parse("[" + productosEnCarro + "]");
 
-  let productosString = JSON.stringify(productosUnicos);
-  productosString = productosString.replace(/[[\]]/g, "");
-  localStorage.setItem("productosEnCarro", productosString);
+    var productosUnicos = productos.filter(
+      (value, index, self) => index === self.findIndex((t) => t.id === value.id)
+    );
 
-  console.log(productos);
-  console.log(productosUnicos);
+    let productosString = JSON.stringify(productosUnicos);
+    productosString = productosString.replace(/[[\]]/g, "");
+    localStorage.setItem("productosEnCarro", productosString);
 
-  productosUnicos.forEach((productos, index) => {
-    fetch(`http://localhost:3000/api/producto/${productos.id}/`)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (producto) {
-        let { id, nombre, imagen, descripcion, precio, tipo_id } =
-          producto.data;
+    let sumaTotal = 0;
+    let totalContenedor = document.createElement("article");
+    let precioTotal = document.createElement("p");
+    let botonesComprar = document.createElement("div");
+    let botonSeguir = document.createElement("button");
+    let botonFinalizar = document.createElement("button");
 
-        const article = document.createElement("article");
-        const imgContainer = document.createElement("div");
-        const img = document.createElement("img");
-        const info = document.createElement("div");
-        const name = document.createElement("p");
-        const desc = document.createElement("p");
-        const cant = document.createElement("input");
-        const cantText = document.createElement("p");
+    productosUnicos.forEach((productos, index) => {
+      fetch(`http://localhost:3000/api/producto/${productos.id}/`)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (producto) {
+          let { id, nombre, imagen, descripcion, precio, tipo_id } =
+            producto.data;
 
-        const priceContainer = document.createElement("div");
+          const article = document.createElement("article");
+          const imgContainer = document.createElement("div");
+          const img = document.createElement("img");
+          const info = document.createElement("div");
+          const name = document.createElement("p");
+          const desc = document.createElement("p");
+          const cant = document.createElement("input");
+          const cantText = document.createElement("p");
 
-        const formEliminar = document.createElement("form");
-        const botonEliminar = document.createElement("button");
+          const priceContainer = document.createElement("div");
+          const numContainer = document.createElement("div");
+          const botonMenos = document.createElement("button");
+          const botonMas = document.createElement("button");
 
-        const price = document.createElement("p");
+          const formEliminar = document.createElement("form");
+          const botonEliminar = document.createElement("button");
 
-        article.setAttribute("class", "detalles-producto");
-        imgContainer.setAttribute("class", "imagen-producto");
-        img.setAttribute("class", "imgVista");
-        info.setAttribute("class", "informacion");
-        name.setAttribute("class", "nombre-producto");
-        desc.setAttribute("class", "descripcion-producto");
-        cant.setAttribute("class", "cantidad-producto");
-        cantText.setAttribute("class", "cantidad-texto");
+          const price = document.createElement("p");
+          const subtotal = document.createElement("p");
 
-        priceContainer.setAttribute("class", "precio-producto");
+          article.setAttribute("class", "detalles-producto");
+          imgContainer.setAttribute("class", "imagen-producto");
+          img.setAttribute("class", "imgVista");
+          info.setAttribute("class", "informacion");
+          name.setAttribute("class", "nombre-producto");
+          desc.setAttribute("class", "descripcion-producto");
+          cant.setAttribute("class", "cantidad-producto");
+          cant.setAttribute("id", "num" + id);
+          subtotal.setAttribute("id", "sub" + id);
 
-        // formEliminar.setAttribute("action", "eliminar");
-        formEliminar.setAttribute("class", "form-eliminar");
-        botonEliminar.setAttribute("type", "submit");
-        botonEliminar.setAttribute("class", "eliminar");
-        botonEliminar.setAttribute("id", id);
-        botonEliminar.innerHTML = "Eliminar";
+          cantText.setAttribute("class", "cantidad-texto");
+          priceContainer.setAttribute("class", "precio-producto");
+          numContainer.setAttribute("class", "num-container");
 
-        name.innerHTML = nombre;
-        price.innerHTML = "$" + precio;
-        desc.innerHTML = descripcion;
-        cant.setAttribute("value", productosUnicos[index].cantidad);
-        cantText.innerHTML = "Cantidad: ";
+          botonMenos.setAttribute("class", "boton-menos");
+          botonMas.setAttribute("class", "boton-mas");
+          botonMenos.setAttribute("id", "menos" + id);
+          botonMas.setAttribute("id", "mas" + id);
+          botonMenos.innerHTML = "-";
+          botonMas.innerHTML = "+";
 
-        container.appendChild(article);
-        article.appendChild(imgContainer);
-        article.appendChild(info);
-        article.appendChild(priceContainer);
-        priceContainer.appendChild(price);
-        priceContainer.appendChild(cantText);
+          // formEliminar.setAttribute("action", "eliminar");
+          formEliminar.setAttribute("class", "form-eliminar");
+          botonEliminar.setAttribute("type", "submit");
+          botonEliminar.setAttribute("class", "eliminar");
+          botonEliminar.setAttribute("id", id);
+          botonEliminar.innerHTML = "Eliminar";
 
-        priceContainer.appendChild(cant);
-        priceContainer.appendChild(formEliminar);
+          name.innerHTML = nombre;
+          price.setAttribute("id", "precio" + id);
+          price.innerHTML = "$" + precio;
+          desc.innerHTML = descripcion;
+          cant.setAttribute("value", productosUnicos[index].cantidad);
+          cantText.innerHTML = "Cantidad: ";
+          subtotal.setAttribute("class", "sub-texto");
+          subtotal.setAttribute(
+            "value",
+            productosUnicos[index].cantidad * precio
+          );
 
-        info.appendChild(name);
-        info.appendChild(desc);
+          subtotal.innerHTML =
+            "Subtotal $" + productosUnicos[index].cantidad * precio;
+          sumaTotal = productosUnicos[index].cantidad * precio + sumaTotal;
+          if (index == productosUnicos.length - 1) {
+            precioTotal.innerHTML = "TOTAL: $" + sumaTotal;
+          }
+          container.appendChild(article);
+          article.appendChild(imgContainer);
+          article.appendChild(info);
+          article.appendChild(priceContainer);
+          priceContainer.appendChild(price);
+          priceContainer.appendChild(cantText);
+          priceContainer.appendChild(numContainer);
+          priceContainer.appendChild(subtotal);
 
-        imgContainer.appendChild(img);
-        img.setAttribute("src", "/img/productos/" + imagen);
+          numContainer.appendChild(botonMenos);
+          numContainer.appendChild(cant);
+          numContainer.appendChild(botonMas);
+          priceContainer.appendChild(formEliminar);
 
-        formEliminar.appendChild(botonEliminar);
-      });
-  });
+          info.appendChild(name);
+          info.appendChild(desc);
 
-  window.addEventListener("click", function (e) {
-    if (e.target.classList.value == "eliminar") {
-      let id = e.target.id;
+          imgContainer.appendChild(img);
+          img.setAttribute("src", "/img/productos/" + imagen);
 
-      let productosFiltro = productosUnicos.filter(
-        (productosUnicos) => productosUnicos.id != id
-      );
+          formEliminar.appendChild(botonEliminar);
+        });
+    });
 
-      productosFiltro = JSON.stringify(productosFiltro);
-      console.log(productosFiltro);
-      productosFiltro = productosFiltro.replace(/[[\]]/g, "");
-      console.log(productosFiltro);
+    totalContenedor.setAttribute("class", "confirmacion-compra");
+    precioTotal.setAttribute("class", "precio-total");
+    botonesComprar.setAttribute("class", "botones-compra");
+    botonSeguir.setAttribute("onclick", "window.location.href='/'");
+    botonFinalizar.setAttribute("class", "precio-total");
+    botonSeguir.innerHTML = "Seguir Comprando";
+    botonFinalizar.innerHTML = "Finalizar compra";
 
-      localStorage.setItem("productosEnCarro", productosFiltro);
-      // e.preventDefault();
-    }
-  });
+    root.appendChild(totalContenedor);
+    totalContenedor.appendChild(precioTotal);
+    totalContenedor.appendChild(botonesComprar);
+    botonesComprar.appendChild(botonSeguir);
+    botonesComprar.appendChild(botonFinalizar);
+
+    window.addEventListener("click", function (e) {
+      if (e.target.classList.value == "eliminar") {
+        let id = e.target.id;
+
+        let productosFiltro = productosUnicos.filter(
+          (productosUnicos) => productosUnicos.id != id
+        );
+        productosFiltro = JSON.stringify(productosFiltro);
+        productosFiltro = productosFiltro.replace(/[[\]]/g, "");
+
+        localStorage.setItem("productosEnCarro", productosFiltro);
+        // e.preventDefault();
+      }
+      if (e.target.classList.value == "boton-mas") {
+        let id = e.target.id;
+        let idNum = id.slice(3);
+        let numSel = document.querySelector("#num" + idNum);
+        let nuevoNum = parseInt(numSel.value) + 1;
+        let subSel = document.querySelector("#sub" + idNum);
+        let precioUni = document.querySelector("#precio" + idNum);
+        let precioNum = precioUni.innerHTML.slice(1);
+
+        subSel.innerHTML = "Subtotal $" + nuevoNum * precioNum;
+        subSel.setAttribute("value", nuevoNum * precioNum);
+        numSel.setAttribute("value", nuevoNum);
+
+        let totalNum = 0;
+        let subTodos = document.querySelectorAll(".sub-texto");
+        console.log(subTodos);
+        subTodos.forEach((element) => {
+          totalNum = parseInt(element.attributes.value.nodeValue) + totalNum;
+        });
+        precioTotal.innerHTML = "TOTAL: $" + totalNum;
+        productosUnicos.forEach((element) => {
+          if (element.id == idNum) {
+            element.cantidad = nuevoNum;
+          }
+        });
+
+        let productosCantidades = JSON.stringify(productosUnicos);
+        productosCantidades = productosCantidades.replace(/[[\]]/g, "");
+        localStorage.setItem("productosEnCarro", productosCantidades);
+      }
+      if (e.target.classList.value == "boton-menos") {
+        let id = e.target.id;
+        let idNum = id.slice(5);
+        let numSel = document.querySelector("#num" + idNum);
+        if (parseInt(numSel.value) > 1) {
+          let nuevoNum = parseInt(numSel.value) - 1;
+          let subSel = document.querySelector("#sub" + idNum);
+          let precioUni = document.querySelector("#precio" + idNum);
+          let precioNum = precioUni.innerHTML.slice(1);
+          subSel.innerHTML = "Subtotal $" + nuevoNum * precioNum;
+          subSel.setAttribute("value", nuevoNum * precioNum);
+          numSel.setAttribute("value", nuevoNum);
+          let totalNum = 0;
+          let subTodos = document.querySelectorAll(".sub-texto");
+          console.log(subTodos);
+          subTodos.forEach((element) => {
+            totalNum = parseInt(element.attributes.value.nodeValue) + totalNum;
+          });
+          precioTotal.innerHTML = "TOTAL: $" + totalNum;
+          productosUnicos.forEach((element) => {
+            if (element.id == idNum) {
+              element.cantidad = nuevoNum;
+            }
+          });
+
+          let productosCantidades = JSON.stringify(productosUnicos);
+          productosCantidades = productosCantidades.replace(/[[\]]/g, "");
+          localStorage.setItem("productosEnCarro", productosCantidades);
+        }
+      }
+    });
+  } else {
+    let contenedorAviso = document.createElement("div");
+    let contenedorTexto = document.createElement("div");
+    let h1 = this.document.createElement("h1");
+    let h3 = this.document.createElement("h3");
+    let imagenLogo = this.document.createElement("img");
+    h1.innerHTML = "¡No hay productos en tu carrito!";
+    h3.innerHTML = "Recuerda agregar los productos para poder comprar";
+
+    contenedorAviso.setAttribute("class", "contenedorAviso");
+
+    imagenLogo.setAttribute("src", "/img/assets/logoPetShopNar.png");
+
+    container.append(contenedorAviso);
+    contenedorAviso.appendChild(contenedorTexto);
+    contenedorTexto.appendChild(h1);
+    contenedorTexto.appendChild(h3);
+    contenedorAviso.appendChild(imagenLogo);
+  }
 });
